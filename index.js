cc7d23b877d87a1772f6749f97e53340ca2b32f4
Damindu Sandeepa <72655390+damindux@users.noreply.github.com>
2023-12-31 19:29:54 +0530

    Improve overall design of the game
    - Add a modal to show who won the game
    - Add an orange accent to highlight what beats what
    - Refine the game reset logic

--- index.js
+++ index.js
@@ -11,10 +11,15 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
-    document.getElementById("player-text").innerHTML = playerSelection;
-    document.getElementById("computer-text").innerHTML = computerSelection;
+    let pSelect = document.getElementById("player-text");
+    let cSelect = document.getElementById("computer-text");
+
+    pSelect.innerHTML = playerSelection;
+    cSelect.innerHTML = computerSelection;
 
     if (playerSelection === computerSelection) {
+        pSelect.style.color = "white";
+        cSelect.style.color = "white";
         return ["It's a Tie!", -1];
     }
 
@@ -22,8 +27,12 @@ function playRound(playerSelection, computerSelection) {
         || (playerSelection === "paper" && computerSelection === "scissors")
         || (playerSelection === "scissors" && computerSelection === "rock")) {
 
+        cSelect.style.color = "orange";
+        pSelect.style.color = "white";
         return [`You Lose! ${computerSelection} beats ${playerSelection}`, 0];
     } else {
+        pSelect.style.color = "orange";
+        cSelect.style.color = "white";
         return [`You Won! ${playerSelection} beats ${computerSelection}`, 1];
     }
 }
@@ -32,6 +41,9 @@ function game() {
     let playerScore = 0;
     let computerScore = 0;
     let textOfGame = document.getElementById("game-text");
+    let modal = document.getElementById("winner-modal");
+    let span = document.getElementsByClassName("close")[0];
+    let winText = document.getElementById("winner-text");
 
     function handleRound(playerSelection) {
         let [gameText, winOrLose] = playRound(playerSelection, getComputerChoice());
@@ -44,17 +56,43 @@ function game() {
             computerScore++;
         }
 
-        if (playerScore === 5 || computerScore === 5) {
-            playerScore = 0;
-            computerScore = 0;
-            if (computerScore === 5)
-                textOfGame.innerHTML = "Computer Won the Game!";
-            else
-                textOfGame.innerHTML = "Player Won the Game!";
-        }
-
         document.getElementById("player-score").innerHTML = playerScore;
         document.getElementById("computer-score").innerHTML = computerScore;
+
+        if (playerScore === 5 || computerScore === 5) {
+            displayModal(playerScore, computerScore);
+        }
+
+        function displayModal(playerScore, computerScore) {
+            let winner = playerScore === 5 ? "Player" : "Computer";
+            winText.innerHTML = `${winner} Wins!`;
+            modal.style.display = "block";
+
+            span.addEventListener("click", function () {
+                modal.style.display = "none";
+                resetGame();
+            });
+
+            window.addEventListener("click", function (e) {
+                if (e.target == modal) {
+                    modal.style.display = "none";
+                    resetGame();
+                }
+            });
+        }
+
+        function resetGame() {
+            playerScore = 0;
+            computerScore = 0;
+            document.getElementById("player-score").innerHTML = playerScore;
+            document.getElementById("computer-score").innerHTML = computerScore;
+            document.getElementById("player-text").innerHTML = "Player";
+            document.getElementById("computer-text").innerHTML = "Computer";
+            document.getElementById("player-text").style.color = "white";
+            document.getElementById("computer-text").style.color = "white";
+            textOfGame.innerHTML = "Click your choice to start!";
+        }
+
     }
 
     document.getElementById("rock").addEventListener("click", function () {
